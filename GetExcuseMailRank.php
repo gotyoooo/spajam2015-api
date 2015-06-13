5af229411a63c6b72545730fb5e6eb6040dff0a5
gotyoooo <lss.ken8927@gmail.com>
2015-06-14 03:10:55 +0900

    利用回数も返すようにした

--- GetExcuseMailRank.php
+++ GetExcuseMailRank.php
@@ -2,7 +2,7 @@
 require_once 'lib/common.inc';
 
 $pdo = db_connect();
-$sql = 'SELECT a.excuse_mail_id as excuse_mail_id,excuse_mail_subject,excuse_mail_message FROM excuse_mail as a LEFT OUTER JOIN excuse_mail_rank as b ON a.excuse_mail_id = b.excuse_mail_id ORDER BY request_num DESC LIMIT 10;';
+$sql = 'SELECT a.excuse_mail_id as excuse_mail_id,excuse_mail_subject,excuse_mail_message,request_num FROM excuse_mail as a LEFT OUTER JOIN excuse_mail_rank as b ON a.excuse_mail_id = b.excuse_mail_id ORDER BY request_num DESC LIMIT 10;';
 $ret = array();
 $data_excuse_mail = db_select_query($pdo, $sql, $ret);
 
--- GetExcuseTelRank.php
+++ GetExcuseTelRank.php
@@ -2,7 +2,7 @@
 require_once 'lib/common.inc';
 
 $pdo = db_connect();
-$sql = 'SELECT a.excuse_tel_id as excuse_tel_id,excuse_tel_name,excuse_tel_group FROM excuse_tel as a LEFT OUTER JOIN excuse_tel_rank as b ON a.excuse_tel_id = b.excuse_tel_id ORDER BY request_num DESC LIMIT 10;';
+$sql = 'SELECT a.excuse_tel_id as excuse_tel_id,excuse_tel_name,excuse_tel_group,request_num FROM excuse_tel as a LEFT OUTER JOIN excuse_tel_rank as b ON a.excuse_tel_id = b.excuse_tel_id ORDER BY request_num DESC LIMIT 10;';
 $ret = array();
 $data_excuse_tel = db_select_query($pdo, $sql, $ret);
 
