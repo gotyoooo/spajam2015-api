1d32e2f1b1ccde58b10f400f2e39daf0c5f23587
gotyoooo <lss.ken8927@gmail.com>
2015-06-13 18:09:43 +0900

    typo

--- GetUserId.php
+++ GetUserId.php
@@ -6,7 +6,7 @@ $sql = 'SELECT MAX(user_id) AS MAX_ID FROM user;';
 $ret = array();
 $max_user_id = db_select_query($pdo, $sql, $ret);
 
-$new_user_id = $max_user_id['MAX_ID'];
+$new_user_id = $max_user_id[0]['MAX_ID'];
 
 $return = array();
 $return['user_id'] = $new_user_id;
