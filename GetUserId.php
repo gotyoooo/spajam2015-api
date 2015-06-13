ab2418bdc216f2c27f204b6e95fbd5e1642697b0
gotyoooo <lss.ken8927@gmail.com>
2015-06-13 18:14:16 +0900

    insert追加

--- GetUserId.php
+++ GetUserId.php
@@ -6,7 +6,12 @@ $sql = 'SELECT MAX(user_id) AS MAX_ID FROM user;';
 $ret = array();
 $max_user_id = db_select_query($pdo, $sql, $ret);
 
-$new_user_id = $max_user_id[0]['MAX_ID'];
+$new_user_id = $max_user_id[0]['MAX_ID'] + 1;
+
+$sql = 'INSERT INTO user(user_id) VALUES(?);';
+$ret = array($new_user_id);
+db_update_query($pdo, $sql, $ret);
+$pdo = null;
 
 $return = array();
 $return['user_id'] = $new_user_id;
