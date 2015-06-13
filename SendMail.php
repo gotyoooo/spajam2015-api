10a031f9da3c2f4f379da30752e48e43b84d0928
gotyoooo <lss.ken8927@gmail.com>
2015-06-13 17:11:21 +0900

    testmail

--- SendMail.php
+++ SendMail.php
@@ -1,8 +1,16 @@
 <?php
 require_once 'lib/common.inc';
 
+$mailFrom = "hoge@gmail.com";
+$mailTo = "lss.ken8927@gmail.com";
+$subject = "test"
+$message = "hoge";
 $smtp_option = array(
     'From' => $mailFrom,
     'To' => array($mailTo)
 );
 $smtp  = new SmtpSender(SMTP_SERVER, $smtp_option);
+$smtp->setSubject($subject);
+if (! $smtp->sendMail($message)) {
+    throw new Exception('Mail send failed.' . $smtp->error['errorMessage']);
+}
