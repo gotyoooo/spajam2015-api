72807e5d2bbf8bf3c0c4aad9586449dacd2b43e8
gotyoooo <lss.ken8927@gmail.com>
2015-06-13 18:08:21 +0900

    json追加

--- GetUserId.php
+++ GetUserId.php
@@ -2,8 +2,18 @@
 require_once 'lib/common.inc';
 
 $pdo = db_connect();
-$sql = 'SELECT MAX(user_id) FROM user;';
+$sql = 'SELECT MAX(user_id) AS MAX_ID FROM user;';
 $ret = array();
 $max_user_id = db_select_query($pdo, $sql, $ret);
 
-var_dump($max_user_id);
+$new_user_id = $max_user_id['MAX_ID'];
+
+$return = array();
+$return['user_id'] = $new_user_id;
+
+//JSONにエンコード
+$json = json_encode( $return );
+
+//JSON形式で吐き出す
+header( 'Content-Type: text/javascript; charset=utf-8' );
+echo $json;
