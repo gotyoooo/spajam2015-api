396499f8930a61d7a644646995e541c51ac90178
gotyoooo <lss.ken8927@gmail.com>
2015-06-14 11:36:11 +0900

    phpバグ修正

--- SetExcuseMail.php
+++ SetExcuseMail.php
@@ -7,14 +7,14 @@ $excuse_mail_message = $_GET['excuse_mail_message'];
 
 $sql = "INSERT INTO excuse_mail(user_id,excuse_mail_subject,excuse_mail_message) VALUES(?,'?','?');";
 $ret = array($user_id, $excuse_mail_subject, $excuse_mail_message);
-$sql2 = "SELECT excuse_mail_id FROM excuse_mail WHERE user_id = ? ORDER BY excuse_mail_id DESC";
+$sql2 = "SELECT max(excuse_mail_id) as max_excuse_mail_id FROM excuse_mail WHERE user_id = ?";
 $ret2 = array($user_id);
 $sql3 = "INSERT INTO excuse_mail_rank(excuse_mail_id, request_num) VALUES(?, ?);";
 
 $pdo = db_connect();
 db_update_query($pdo, $sql, $ret);
 $data_excuse_mail = db_select_query($pdo, $sql2, $ret2);
-$excuse_mail_id = $data_excuse_mail[0]['excuse_mail_id'];
+$excuse_mail_id = $data_excuse_mail[0]['max_excuse_mail_id'];
 $ret3 = array($excuse_mail_id, 1);
 db_update_query($pdo, $sql3, $ret3);
 $pdo = null;
--- SetExcuseTel.php
+++ SetExcuseTel.php
@@ -7,14 +7,14 @@ $excuse_tel_group = $_GET['excuse_tel_group'];
 
 $sql = "INSERT INTO excuse_tel(user_id,excuse_tel_name,excuse_tel_group) VALUES(?,'?','?');";
 $ret = array($user_id, $excuse_tel_name, $excuse_tel_group);
-$sql2 = "SELECT excuse_tel_id FROM excuse_tel WHERE user_id = ? ORDER BY excuse_tel_id DESC";
+$sql2 = "SELECT max(excuse_tel_id) as max_excuse_tel_id FROM excuse_tel WHERE user_id = ?";
 $ret2 = array($user_id);
 $sql3 = "INSERT INTO excuse_tel_rank(excuse_tel_id, request_num) VALUES(?, ?);";
 
 $pdo = db_connect();
 db_update_query($pdo, $sql, $ret);
 $data_excuse_tel = db_select_query($pdo, $sql2, $ret2);
-$excuse_tel_id = $data_excuse_tel[0]['excuse_tel_id'];
+$excuse_tel_id = $data_excuse_tel[0]['max_excuse_tel_id'];
 $ret3 = array($excuse_tel_id, 1);
 db_update_query($pdo, $sql3, $ret3);
 $pdo = null;
